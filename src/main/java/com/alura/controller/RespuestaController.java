48db44d537d1afcb29583034c5b617ea194cad4e
yohan <yohamanu23@gmail.com>
2023-05-30 14:03:56 -0500

    Se desarrollo el metodo GET para listar los topicos con sus respuestas

--- src/main/java/com/alura/controller/RespuestaController.java
+++ src/main/java/com/alura/controller/RespuestaController.java
@@ -5,6 +5,7 @@ import com.alura.domain.respuestas.DatosRegistroRespuesta;
 import com.alura.domain.respuestas.Respuesta;
 import com.alura.domain.respuestas.RespuestaRepository;
 import com.alura.domain.topico.DatosTopicoConRespuestas;
+import com.alura.domain.topico.Topico;
 import com.alura.domain.topico.TopicoRepository;
 import com.alura.domain.usuarios.DatosMostrarUsuario;
 import com.alura.domain.usuarios.UsuarioRepository;
@@ -16,6 +17,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/respuestas")
@@ -25,7 +27,7 @@ public class RespuestaController {
     private final TopicoRepository topicoRepository;
     private final UsuarioRepository usuarioRepository;
 
-@Autowired
+    @Autowired
     public RespuestaController(RespuestaRepository respuestaRepository, TopicoRepository topicoRepository, UsuarioRepository usuarioRepository) {
         this.respuestaRepository = respuestaRepository;
         this.topicoRepository = topicoRepository;
@@ -41,12 +43,29 @@ public class RespuestaController {
         var respuesta = respuestaRepository.save(new Respuesta(datosRegistroRespuesta, topico, autor));
         var datosMostarUsuario = new DatosMostrarUsuario(autor.getId(), autor.getNombre());
         var datosMostrarRespuesta = new DatosMostrarRespuesta(respuesta.getId(), respuesta.getMensaje(),
-                respuesta.getFechaCreacion(), datosMostarUsuario,respuesta.getSolucion());
+                respuesta.getFechaCreacion(), datosMostarUsuario, respuesta.getSolucion());
         DatosTopicoConRespuestas datosTopicoConRespuestas = new DatosTopicoConRespuestas(topico.getId(),
-                topico.getTitulo(), topico.getMensaje(), topico.getFechaCreacion(),topico.getStatus(),datosMostarUsuario,
+                topico.getTitulo(), topico.getMensaje(), topico.getFechaCreacion(), topico.getStatus(), datosMostarUsuario,
                 topico.getCurso(), List.of(datosMostrarRespuesta));
-        URI url= uriComponentsBuilder.path("/{idTopico}/{idRespuesta}").buildAndExpand(topico.getId().toString(),
+        URI url = uriComponentsBuilder.path("/{idTopico}/{idRespuesta}").buildAndExpand(topico.getId().toString(),
                 respuesta.getId().toString()).toUri();
         return ResponseEntity.created(url).body(datosTopicoConRespuestas);
+    }
+
+    @GetMapping("/{idTopico}")
+    public ResponseEntity<DatosTopicoConRespuestas> listarTopicoConRespuestas(@PathVariable Long idTopico) {
+        Topico topico = topicoRepository.getReferenceById(idTopico);
+        var respuestas = respuestaRepository.findByTopicoId(idTopico);
+
+        List<DatosMostrarRespuesta> datosRespuestas = respuestas.stream()
+                .map(respuesta -> new DatosMostrarRespuesta(respuesta.getId(), respuesta.getMensaje(),
+                        respuesta.getFechaCreacion(), new DatosMostrarUsuario(respuesta.getAutor().getId(),
+                        respuesta.getAutor().getNombre()), respuesta.getSolucion()))
+                .collect(Collectors.toList());
+        DatosTopicoConRespuestas datosTopicoConRespuestas = new DatosTopicoConRespuestas(topico.getId(), topico.getTitulo(),
+                topico.getMensaje(), topico.getFechaCreacion(), topico.getStatus(),
+                new DatosMostrarUsuario(topico.getAutor().getId(), topico.getAutor().getNombre()),
+                topico.getCurso(), datosRespuestas);
+        return ResponseEntity.ok(datosTopicoConRespuestas);
     }
 }
