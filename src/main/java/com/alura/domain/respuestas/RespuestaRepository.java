e206404a852b630a267a74a01e70b2cf2ffb2f3f
yohan <yohamanu23@gmail.com>
2023-05-30 14:03:10 -0500

    Se desarrollo el metodo findByTopicoId

--- src/main/java/com/alura/domain/respuestas/RespuestaRepository.java
+++ src/main/java/com/alura/domain/respuestas/RespuestaRepository.java
@@ -3,6 +3,9 @@ package com.alura.domain.respuestas;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RespuestaRepository extends JpaRepository<Respuesta,Long> {
+    List<Respuesta> findByTopicoId(Long idTopico);
 }
